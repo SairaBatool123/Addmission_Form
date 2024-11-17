d80e6586806e4920130912db4cbdad39bdd3fea0
Amar Yasir <3marofficial249@gmail.com>
2024-11-17 14:39:59 +0200

    some improvements !

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
+
 // student form
-document.getElementById('studentForm').addEventListener('submit', function (e) {
+const studentForm = document.getElementById('studentForm');
+studentForm.addEventListener('submit', function (e) {
     e.preventDefault()
-    var name = document.getElementById('name').value
-    var age = document.getElementById('age').value
-    var grade = document.getElementById('class').value
+    let name = document.getElementById('name').value
+    let age = document.getElementById('age').value
+    let grade = document.getElementById('class').value
 
     //table
-    var table = document.getElementById('studentTable').querySelector('tbody')
-    var newTr = table.insertRow();
+    let table = document.getElementById('studentTable').querySelector('tbody')
+    let newTr = table.insertRow();
 
-    var td1 = newTr.insertCell(0)
-    var td2 = newTr.insertCell(1)
-    var td3 = newTr.insertCell(2)
-    var td4 = newTr.insertCell(3)
+    let td1 = newTr.insertCell(0)
+    let td2 = newTr.insertCell(1)
+    let td3 = newTr.insertCell(2)
+    let td4 = newTr.insertCell(3)
 
     // here we get the data from the (variable) of student form 
     td1.innerHTML = name
@@ -20,40 +22,44 @@ document.getElementById('studentForm').addEventListener('submit', function (e) {
     td3.innerHTML = grade
     td4.innerHTML = `<button class="new-btn" onclick="editRow(this)">Edit</button><button class="new-btn" onclick="deleteRow(this)">Delete</button>`
 
-    document.getElementById('studentForm').reset()
+    studentForm.reset()
 
     // end of student form
 })
 function deleteRow(button) {
-    var tr = button.parentNode.parentNode;
+    let tr = button.parentNode.parentNode;
     tr.parentNode.removeChild(tr)
 }
 function editRow(button) {
-    var tr = button.parentNode.parentNode
+    let tr = button.parentNode.parentNode
 
-    var index = tr.rowIndex - 1
-    var name = tr.cells[0].innerHTML
-    var age = tr.cells[1].innerHTML
-    var grade = tr.cells[2].innerHTML
+    let index = tr.rowIndex - 1
+    let name = tr.cells[0].innerHTML
+    let age = tr.cells[1].innerHTML
+    let grade = tr.cells[2].innerHTML
 
-
+    // here we get the data from the (variable) of student form
     document.getElementById('editIndex').value = index
     document.getElementById('editName').value = name
     document.getElementById('editAge').value = age
     document.getElementById('editClass').value = grade
+
     // edit Form 
-    document.getElementById('editForm').classList.remove('hidden')
+    const eidtForm = document.getElementById('editForm')
+    eidtForm.classList.remove('hidden')
 }
-document.getElementById('editStudentDetail').addEventListener('submit', function (e) {
+const editStudentDetail = document.getElementById('editStudentDetail')
+
+editStudentDetail.addEventListener('submit', function (e) {
     e.preventDefault()
 
-    var index = document.getElementById('editIndex').value
-    var name = document.getElementById('editName').value
-    var age = document.getElementById('editAge').value
-    var grade = document.getElementById('editClass').value
+    let index = document.getElementById('editIndex').value
+    let name = document.getElementById('editName').value
+    let age = document.getElementById('editAge').value
+    let grade = document.getElementById('editClass').value
 
-    var table = document.getElementById('studentTable').querySelector('tbody')
-    var tr = table.rows[index]
+    let table = document.getElementById('studentTable').querySelector('tbody')
+    let tr = table.rows[index]
 
     tr.cells[0].innerHTML = name
     tr.cells[1].innerHTML = age
